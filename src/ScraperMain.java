eddf7f3a44bd718ead123054afae1299f44ba8c4
Mark Davies <m.davies6@EDU.epsb.ca>
2019-02-04 13:17:26 -0700

    basic scraper running
    removed Couprie.java

--- src/ScraperMain.java
+++ src/ScraperMain.java
@@ -1,6 +1,34 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class ScraperMain {
 
     public static void main(String[] args) {
-        System.out.println("f");
+
+        try {
+
+            Document doc;
+            doc = Jsoup.connect("https://www.kijiji.ca/b-edmonton/nissan-frontier/k0l1700203?dc=true").get();
+
+//            String title = doc.title();
+//            System.out.println("Title: " + title);
+//            System.out.println("");
+
+            Elements prices = doc.select(".price");
+
+            int total = 0;
+
+            for (Element price : prices) {
+                System.out.println("Price: " + price.text());
+
+            }
+
+        } catch (IOException e) {
+            System.err.println(e);
+        }
     }
 }

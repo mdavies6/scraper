5c68d0716b88869d29be52f4021b017690b947f0
jacobgerun1 <44880481+jacobgerun1@users.noreply.github.com>
2019-02-08 13:25:31 -0700

    add GUI

---
+++ src/ScraperGUI.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class ScraperGUI {
+    private JTextField scraperTextField;
+    private JPanel panel1;
+    private JButton searchButton;
+}
---
+++ src/SearchGUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class SearchGUI {
+    private JTextField searchTextField;
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+}

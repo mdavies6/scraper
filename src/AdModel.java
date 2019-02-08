df96a61c307f53cfe44527d6adfd1bef08405036
Mark Davies <m.davies6@EDU.epsb.ca>
2019-02-08 13:28:30 -0700

    add prices

--- src/AdModel.java
+++ src/AdModel.java
@@ -1,21 +1,50 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
 
 public class AdModel {
 
-    public String title;
     public String url;
+
+    public String title;
     public double listedPrice;
     public String text;
 
     public int wordCount;
 
-public AdModel(String title, String url, double listedPrice, String text){
-    this.title = title;
+public AdModel(String url){
     this.url = url;
-    this.listedPrice = listedPrice;
-    this.text = text;
 
-    calcWordCount();
+    listedPrice = getPrice();
+    //calcWordCount();
+}
+
+private double getPrice(){
+    try {
+
+        Document doc;
+        doc = Jsoup.connect(url).get();
+
+        Element priceElement = doc.select("span[content]").first();
+        String priceString = priceElement.attr("content");
+
+        double price = Double.parseDouble(priceString);
+        System.out.println(price);
+
+        return price;
+
+    } catch (IOException | NullPointerException e) {
+        if(e instanceof NullPointerException){
+
+        }else{
+            System.err.println(e);
+        }
+
+    }//end try-catch
+    return -1.0;
 }
 
     private void calcWordCount() {

66e1732a82674b36336c1cb7407fd770a15e9a02
jacobgerun1 <44880481+jacobgerun1@users.noreply.github.com>
2019-02-14 13:04:03 -0700

    Button Listener

--- src/ScraperGUI.java
+++ src/ScraperGUI.java
@@ -1,7 +1,21 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ScraperGUI {
     private JTextField scraperTextField;
     private JPanel panel1;
     private JButton searchButton;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+    public ScraperGUI() {
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
 }
--- src/SearchGUI.java
+++ src/SearchGUI.java
@@ -6,4 +6,11 @@ public class SearchGUI {
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
+    private JTable resultsTable;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        String[] columnHeaders = {"Title","Listed Price", "Description"};
+        Object [][] data = {};
+    }
 }

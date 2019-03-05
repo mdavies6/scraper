807f82a2a82cb39cec54ac2765af39e5c6d41b97
Mark Davies <m.davies6@EDU.epsb.ca>
2019-03-05 13:01:41 -0700

    Update ResultsGUI.java

--- src/ResultsGUI.java
+++ src/ResultsGUI.java
@@ -1,26 +1,19 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import java.util.ArrayList;
 
 public class ResultsGUI extends JFrame {
     private JPanel panel1;
     private JTable table1;
+    public ArrayList<AdModel> modelList;
 
-    public ResultsGUI(AdModel ad) {
+    public ResultsGUI(ArrayList<AdModel> modelList) {
+        this.modelList = modelList;
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(600, 800);
-        setupTable(ad);
         this.add(panel1);
 
     }
 
-    public void setupTable(AdModel tempAd) {
-        String column[] = {"Title", "Price", "Word Count"};
-        TableModel tableModel = new DefaultTableModel(column, 0);
-
-        Object[] obj = {tempAd.title, tempAd.listedPrice, tempAd.wordCount};
-        ((DefaultTableModel) tableModel).addRow(obj);
-
-        table1 = new JTable(tableModel);
-    }
 }
